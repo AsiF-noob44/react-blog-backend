65876a41d15c8e1988255c6c152dbda6ddf6e756
Abu Sayeed Rifat <abusayeedrifat20@gmail.com>
2026-01-12 02:39:17 +0600

    3rd commit

--- server.js
+++ server.js
@@ -9,10 +9,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: 'https://react-blog-byasif.netlify.app',
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "https://react-blog-byasif.netlify.app",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 connectDB();
