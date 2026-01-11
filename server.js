efd34fbf8c2f48c5fe88d44031da08c60ba45411
Abu Sayeed Rifat <abusayeedrifat20@gmail.com>
2026-01-12 02:37:45 +0600

    Restrict CORS to frontend origin

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://react-blog-byasif.netlify.app',
+  credentials: true
+}));
 app.use(express.json());
 
 connectDB();
